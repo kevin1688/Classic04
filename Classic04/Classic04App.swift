764581dce79bac3c93dff40d514ec3839d3c00d9
kb18k <jackexi@gmail.com>
2021-09-14 20:40:18 +0800

    Initial Commit

---
+++ Classic04/Classic04App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic04App.swift
+//  Classic04
+//
+//  Created by kai wen chen on 2021/9/14.
+//
+
+import SwiftUI
+
+@main
+struct Classic04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
